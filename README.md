e6bea046dc3dfda47bd6b5e231ec472a120b4a8d
w1tch3r-code <rainerbrain@gmail.com>
2023-10-30 15:04:37 +0100

    JS Optimalgewichtsrechner samt README.md fertig gestellt

---
+++ README.md
@@ -0,0 +1,41 @@
+# Idealgewichts-Rechner – JavaScript-Projekt
+
+## Beschreibung
+
+Dieses JavaScript-Projekt wurde im Rahmen eines Coding-Bootcamps bei supercode.de erstellt. Es handelt sich um einen Idealgewichts-Rechner, der es ermöglicht, das Optimalgewicht, abhängig vom jeweiligen Körperbau des Users, zu berechen.
+
+Die Anwendung bietet die Möglichkeit, die folgenden Berechnungen durchzuführen:
+
+- Idealgewicht für einen breiten Körperbau
+- Idealgewicht für einen zierlichen und schmalen Körperbau
+
+Das Idealgewicht wird anhand folgender Formeln berechnet:
+- Formel für Menschen mit einem zierlichen und schmalen Körperbau:
+[(Körpergröße in cm - 100) + (Alter/ 10)] x 0,9 x 0,9
+
+- Formel für Menschen mit einem eher breiten Körperbau:
+[(Körpergröße in cm - 100) + (Alter/ 10)] x 0,9 x 1,1
+
+
+## Funktionsweise
+
+1. Gib deine Körpergröße in cm an.
+2. Gib dein Alter in Jahren an.
+3. Wähle aus, ob du einen breiten oder schmalen Körperbau hast.
+6. Klicke auf den Berechnen-Button, um die Ergebnisse anzeigen zu lassen.
+
+## Screenshots
+
+![PAP - Flowchart für den Idealgewichtsrechner](./assets/images/screenshot_flowchart_idealgewichtsrechner.jpg)
+
+![Idealgewicht von einer Person mit schmalerem Körperbau](./assets/images/screenshot_idealgewichtsrechner.jpg)
+
+## GitHub Repository
+
+Du findest das gesamte Projekt auf GitHub: [GitHub Repository](https://w1tch3r-code.github.io/js_optimalgewicht_rechner/)
+
+## Anmerkungen
+
+- Dieses Projekt wurde im Rahmen eines Coding-Bootcamps bei supercode.de erstellt.
+- Die Anwendung ermöglicht eine einfache und schnelle Berechnung des Idealgewichts abhängig vom jeweiligen Körperbau.
+- Die README.md-Datei dient auch als Bewerbungsunterlage und präsentiert meine Fähigkeiten in der Webentwicklung.
---
+++ assets/js/main.js
@@ -0,0 +1,64 @@
+"use strict";
+
+// ===================================================
+//   JS-Vertiefung – Project Optimalgewicht-Rechner
+// ===================================================
+
+console.log("%c JS-Vertiefung – Project Optimalgewicht-Rechner", "color: tomato");
+
+// ===================================================
+//    	 		Berechnung des Idealgewichts
+// ===================================================
+
+const idealWeight = () => {
+	// Input Height
+	const inputHeight = Number(document.querySelector("#height").value);
+
+	// Input Age
+	const inputAge = Number(document.querySelector("#age").value);
+
+	// Radio Buttons - wide/slim
+	let rdBtnPhysique = document.querySelectorAll("input[name='physique']");
+
+	if (inputHeight === 0 || inputAge === 0 ){
+		return false;
+	} else {
+		if (rdBtnPhysique[0].checked) {
+			// Formel für Menschen mit einem eher breiten Körperbau:[(Körpergröße in cm - 100) + (Alter/ 10)] x 0,9 x 1,1
+	
+			const broadPhysique =
+				(((inputHeight - 100) + (inputAge / 10)) * 0.9 * 1.1).toFixed(2);
+			return broadPhysique;
+	
+		} else if (rdBtnPhysique[1].checked) {
+			// Formel für Menschen mit einem zierlichen und schmalen Körperbau:[(Körpergröße in cm - 100) + (Alter/ 10)] x 0,9 x 0,9
+	
+			const slenderPhysique =
+			(((inputHeight - 100) + (inputAge / 10)) * 0.9 * 0.9).toFixed(2);
+			return slenderPhysique;
+		}
+	}
+};
+
+
+// ===================================================
+//     	  				 Ausgabe 
+// ===================================================
+
+const btnCalc = document.querySelector("#btnCalc");
+
+// Button Klick-Function für idealWeight() Ausgabe
+btnCalc.addEventListener("click", (event) => {
+	event.preventDefault();
+
+	const output = document.querySelector('#output');
+
+	if (idealWeight() === false ) {
+		output.style.display = 'block';
+		output.textContent = 'Bitte füllen Sie alle Felder aus!';
+		return false;
+	} else {
+		// Ausgabe von Idealgewicht
+		output.textContent = `Dein Idealgewicht ist ${idealWeight().replace('.',',')} kg.`;
+	}
+});
